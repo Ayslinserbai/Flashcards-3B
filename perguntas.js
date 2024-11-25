ace1fbce31f42cc69becfdd877c1f7d17cbce8fb
Ayslinserbai <127760070+Ayslinserbai@users.noreply.github.com>
2024-11-25 07:23:40 -0300

    ajustes

--- perguntas.js
+++ perguntas.js
@@ -3,3 +3,33 @@ criaCartao(
     'Quantos ossos tem um corpo humano? E um bêbê?',
     '206 ossos, um bêbê tem 305.'
 )
+
+criaCartao(
+    'ANATOMIA',
+    'Qual a parte mais inferior do corpo?',
+    'A sola do pé.'
+)
+
+criaCartao(
+    'ANATOMIA',
+    'Qual a origem do termo anatomia?',
+    ' Vem do grego ANA-em partes TOMEIN-cortar.'
+)
+
+criaCartao(
+    'ANATOMIA',
+    'O que é anomalia?',
+    'Anomalia é uma alteração morfológica que causa prejuízo funcional.'
+)
+
+criaCartao(
+    'ANATOMIA',
+    'Pneumonia é uma infecção contagiosa?',
+    'Pneumonia não é uma infecção e sim uma doença. Não sendo contagiosa.'
+)
+
+criaCartao(
+    'ANATOMIA',
+    'Qual é o único osso dividido em três?',
+    'Bacia'
+)
